05b5fad7100243c4a57441736276bd7b31a62711
anish-agnihotri <contact@anishagnihotri.com>
2019-03-09 02:50:41 -0500

    Adding server stats queries + front-end

---
+++ client/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <div class="main">
+
+    </div>
+    <div class="footer">
+
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.app {
+  .main {
+    width: 100vw;
+    height: calc(100vh - 120px);
+    background-color: #40409a;
+  }
+  .footer {
+    width: 100vw;
+    min-height: 120px; //fix
+    background-color: #f3f3f3;
+  }
+}
+</style>
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ server/src/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+const port = 8082; // default port to listen
+
+
+// Home page statistics:
+let etherscanAion = 'https://api.cryptonator.com/api/ticker/aion-usd';
+let supply = 'https://api.alternative.me/v2/ticker/Aion/';
+let blockTime = '';
+let networkHash = '';
+
+let aionPrice;
+let aionChange;
+let aionSupply;
+let aionBlockTime;
+let aioNetworkHash;
+
+function getPrice() {
+    request.get(etherscanAion, (error, response, body) => {
+        if (error) {
+            return console.log('There has been an error! Please contact support.');
+        }
+        aionPrice = JSON.parse(body)['ticker']['price'];
+        aionChange = JSON.parse(body)['ticker']['change'];
+    });
+}
+
+function getSupply() {
+    request.get(supply, (error, response, body) => {
+        if (error) {
+            return console.log('There has been an error! Please contact support.');
+        }
+        aionSupply = JSON.parse(body)['data']['2062']['total_supply'];
+    });
+}
+
+/*
+function BlockTime() {
+
+}
+
+function aionNetworkHash() {
+
+}
+*/
+getPrice();
+getSupply();
+
+
+setInterval(function () {
+    getPrice();
+    getSupply();
+}, 30000)
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.json({
+        aion_price: aionPrice,
+        aion_change: aionChange,
+        aion_supply: aionSupply,
+    });
+});
+
+// start the Express server
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${ port }`);
+});
